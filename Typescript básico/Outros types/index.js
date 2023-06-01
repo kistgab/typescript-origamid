aefd90f4aae994d374ec51a11504a58ed05e301b
Gabriel Kist <gabriel.kist@cwi.com.br>
2023-06-01 19:45:20 -0300

    outros tipos

---
+++ Typescript básico/Outros types/index.js
@@ -0,0 +1,14 @@
+"use strict";
+function toNumber(value) {
+    if (typeof value === "number")
+        return value;
+    if (!isNaN(parseFloat(value)))
+        return parseFloat(value);
+    throw Error("value have to be a string containing a number or a number");
+}
+console.log(toNumber(90));
+console.log(toNumber("80"));
+console.log(toNumber("70aaa"));
+console.log(toNumber("a60aaa"));
+console.log(toNumber(" "));
+console.log(toNumber("asdas"));
