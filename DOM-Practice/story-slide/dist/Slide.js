bf3a875faccbf20a59f8e37107f691e984de9cc2
kistgab <gabrielkist02@gmail.com>
2023-08-29 11:45:14 -0300

    feat: adding slides controls

--- DOM-Practice/story-slide/dist/Slide.js
+++ DOM-Practice/story-slide/dist/Slide.js
@@ -1,17 +1,51 @@
 export default class Slide {
     container;
-    elements;
+    slides;
     controls;
     time;
-    constructor(container, elements, controls, time = 5000) {
+    activeIndex;
+    activeSlide;
+    constructor(container, slides, controls, time = 5000) {
         this.container = container;
-        this.elements = elements;
+        this.slides = slides;
         this.controls = controls;
         this.time = time;
-        console.log(this.container);
-        console.log(this.elements);
-        console.log(this.controls);
-        console.log(this.time);
+        this.activeIndex = 0;
+        this.activeSlide = this.slides[this.activeIndex];
+        this.init();
+    }
+    hide(slide) {
+        slide.classList.remove("active");
+    }
+    show(index) {
+        this.slides.forEach(this.hide);
+        this.activeIndex = index;
+        this.activeSlide = this.slides[this.activeIndex];
+        this.activeSlide.classList.add("active");
+    }
+    next() {
+        const totalSlides = this.slides.length;
+        const realActiveSlidePosition = this.activeIndex + 1;
+        const next = realActiveSlidePosition < totalSlides ? this.activeIndex + 1 : 0;
+        this.show(next);
+    }
+    prev() {
+        const prev = this.activeIndex > 0 ? this.activeIndex - 1 : this.slides.length - 1;
+        this.show(prev);
+    }
+    addControls() {
+        const prevButton = document.createElement("button");
+        prevButton.innerText = "Slide Anterior";
+        const nextButton = document.createElement("button");
+        nextButton.innerText = "Próximo Slide";
+        this.controls.appendChild(prevButton);
+        this.controls.appendChild(nextButton);
+        nextButton.addEventListener("pointerup", () => this.next());
+        prevButton.addEventListener("pointerup", () => this.prev());
+    }
+    init() {
+        this.addControls();
+        this.show(this.activeIndex);
     }
 }
 //# sourceMappingURL=Slide.js.map
--- DOM-Practice/story-slide/src/Slide.ts
+++ DOM-Practice/story-slide/src/Slide.ts
@@ -1,23 +1,64 @@
 export default class Slide {
   container: Element;
-  elements: Element[];
+  slides: Element[];
   controls: Element;
   time: number;
+  activeIndex: number;
+  activeSlide: Element;
 
   constructor(
     container: Element,
-    elements: Element[],
+    slides: Element[],
     controls: Element,
     time: number = 5000
   ) {
     this.container = container;
-    this.elements = elements;
+    this.slides = slides;
     this.controls = controls;
     this.time = time;
+    this.activeIndex = 0;
+    this.activeSlide = this.slides[this.activeIndex];
+    this.init();
+  }
 
-    console.log(this.container);
-    console.log(this.elements);
-    console.log(this.controls);
-    console.log(this.time);
+  hide(slide: Element) {
+    slide.classList.remove("active");
+  }
+
+  show(index: number) {
+    this.slides.forEach(this.hide);
+    this.activeIndex = index;
+    this.activeSlide = this.slides[this.activeIndex];
+    this.activeSlide.classList.add("active");
+  }
+
+  next(): void {
+    const totalSlides = this.slides.length;
+    const realActiveSlidePosition = this.activeIndex + 1;
+    const next =
+      realActiveSlidePosition < totalSlides ? this.activeIndex + 1 : 0;
+    this.show(next);
+  }
+
+  prev(): void {
+    const prev =
+      this.activeIndex > 0 ? this.activeIndex - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+
+  private addControls() {
+    const prevButton = document.createElement("button");
+    prevButton.innerText = "Slide Anterior";
+    const nextButton = document.createElement("button");
+    nextButton.innerText = "Próximo Slide";
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+    nextButton.addEventListener("pointerup", () => this.next());
+    prevButton.addEventListener("pointerup", () => this.prev());
+  }
+
+  private init() {
+    this.addControls();
+    this.show(this.activeIndex);
   }
 }
