8765be52a59dff442aef69908ece2c5426d1d1ef
Gabriel Kist <83172938+kistgab@users.noreply.github.com>
2023-08-29 09:48:46 -0300

    Delete Data-Practice/exercise/build directory

--- Data-Practice/exercise/build/currencyToValue.js
+++
@@ -1,7 +0,0 @@
-/**
- * Receive 1.200.500,00 returns 1200500.00
- */
-export function currencyToValue(currencyValue) {
-    const formattedValue = Number(currencyValue.replaceAll(".", "").replaceAll(",", "."));
-    return isNaN(formattedValue) ? null : formattedValue;
-}
--- Data-Practice/exercise/build/stringToDate.js
+++
@@ -1,7 +0,0 @@
-export function stringToDate(text) {
-    const [date, time] = text.split(" ");
-    const [day, month, year] = date.split("/").map(Number);
-    const [hour, minute] = time.split(":").map(Number);
-    const monthIndex = month - 1;
-    return new Date(year, monthIndex, day, hour, minute);
-}
